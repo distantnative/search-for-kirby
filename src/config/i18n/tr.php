87a520edb85e6a3df5a4470e25e5b311f10f998a
Ahmet Bora <byybora@gmail.com>
2020-07-04 16:33:21 +0300

    Add tr language

---
+++ src/config/i18n/tr.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'view.search'  => 'Arama',
+    'search.all'   => 'Tüm arama sonuçları',
+    'search.build' => 'Dizini yeniden oluştur',
+    'search.built' => 'Arama dizini oluşturuldu'
+];
