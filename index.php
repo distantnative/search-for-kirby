59167f72329a6bcc6743c41074450507bc32fb49
Nico Hoffmann <on-github@nhoffmann.com>
2020-11-15 17:21:03 +0100

    🗑 Remove unneeded Panel section

--- index.php
+++ index.php
@@ -4,19 +4,16 @@ use Kirby\Cms\App;
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-App::plugin('distantnative/search-for-kirby', array_merge(
-    [
-        'api'   => require 'src/config/api.php',
-        'hooks' => require 'src/config/hooks.php',
-        'translations' => [
-            'en' => require 'src/config/i18n/en.php',
-            'de' => require 'src/config/i18n/de.php'
-        ],
-        'components' => [
-            'search' => require 'src/config/component.php'
-        ]
+App::plugin('distantnative/search-for-kirby', [
+    'api'   => require 'src/config/api.php',
+    'hooks' => require 'src/config/hooks.php',
+    'translations' => [
+        'en' => require 'src/config/i18n/en.php',
+        'de' => require 'src/config/i18n/de.php'
     ],
-    require 'src/config/panel.php'
-));
+    'components' => [
+        'search' => require 'src/config/component.php'
+    ]
+]);
 
 include_once 'src/config/helpers.php';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import Search from "./panel/Search.vue";
-import Section from "./panel/Section.vue";
 
 panel.plugin("distantnative/search-for-kirby", {
-  sections: {
-    search: Section
-  },
   components: {
     "k-search": Search
   }
